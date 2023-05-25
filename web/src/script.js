b6daf947a85abe65724def8e7c375d3ddb4fd7ba
NicoSoup <mamak06mamak13@gmail.com>
2023-05-25 14:24:35 +0900

    location base 真下

--- web/src/script.js
+++ web/src/script.js
@@ -1,4 +1,13 @@
 function submit() {
-    alert("clicked!!")
+    console.log("submit!!!")
+    navigator.geolocation.getCurrentPosition(function (position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        // 計算した目的地の座標を使用して処理を行う
+        const ele = document.createElement('a-box');
+        ele.setAttribute("material", "color: red")
+        ele.setAttribute("scale", "2 1 2");
+        ele.setAttribute("gps-entity-place", "latitude: " + latitude + "; longitude: " + longitude + ";");
+        document.querySelector('a-scene').appendChild(ele);
+    });
 }
-
